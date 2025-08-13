a69b651cbc30cf3c7f070d99240c865a1aeced98
Rob Stevenson <this.rob@protonmail.com>
2025-08-13 10:10:12 +0100

    feat(server): create database file on server start

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -1,10 +1,16 @@
-use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
+use sqlx::{
+    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+    SqlitePool,
+};
+use std::str::FromStr;
 
 pub mod helpers;
 
 pub async fn init(db_path: &str) -> Result<SqlitePool, sqlx::Error> {
+    let connect_options = SqliteConnectOptions::from_str(db_path)?.create_if_missing(true);
+
     SqlitePoolOptions::new()
         .max_connections(5)
-        .connect(db_path)
+        .connect_with(connect_options)
         .await
 }
