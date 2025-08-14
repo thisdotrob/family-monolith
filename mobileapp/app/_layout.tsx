45bbce3ee5532b8ba8313e2688a436b9de7f198c
Rob Stevenson <this.rob@protonmail.com>
2025-08-14 20:45:57 +0100

    feat(mobileapp): configure basic apollo client

--- mobileapp/app/_layout.tsx
+++ mobileapp/app/_layout.tsx
@@ -2,16 +2,15 @@ import 'react-native-url-polyfill/auto';
 import React from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { Slot } from 'expo-router';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { ApolloProvider } from '@apollo/client';
+import client from './api/apollo';
 
 export default function RootLayout() {
   return (
-    <PaperProvider
-      settings={{
-        icon: (props) => <MaterialCommunityIcons {...props} />,
-      }}
-    >
-      <Slot />
-    </PaperProvider>
+    <ApolloProvider client={client}>
+      <PaperProvider>
+        <Slot />
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
