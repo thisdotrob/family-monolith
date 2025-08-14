7c352da51c5457fab883f6cce4f69371560d6a8a
Rob Stevenson <this.rob@protonmail.com>
2025-08-14 21:00:35 +0100

    feat(mobileapp): implement login mutation

--- mobileapp/app/api/apollo.ts
+++ mobileapp/app/api/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.53:4173/graphql',
+  uri: 'http://192.168.1.53:4173/v1/graphql/auth',
 });
 
 const client = new ApolloClient({
