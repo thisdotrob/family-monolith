7c8c22d4439a168c60a6eb8681a317aee1f7d3e6
Rob Stevenson <this.rob@protonmail.com>
2025-08-14 21:17:07 +0100

    feat(mobileapp): implement AuthContext and token storage

--- mobileapp/app/_layout.tsx
+++ mobileapp/app/_layout.tsx
@@ -1,16 +1,27 @@
-import 'react-native-url-polyfill/auto';
 import React from 'react';
 import { PaperProvider } from 'react-native-paper';
-import { Slot } from 'expo-router';
 import { ApolloProvider } from '@apollo/client';
+import { AuthProvider } from '../src/contexts/AuthContext';
+import LoginPage from '../src/pages/LoginPage';
 import client from './api/apollo';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <ApolloProvider client={client}>
-      <PaperProvider>
-        <Slot />
-      </PaperProvider>
-    </ApolloProvider>
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <PaperProvider>
+          <SafeAreaView style={styles.container}>
+            <LoginPage />
+          </SafeAreaView>
+        </PaperProvider>
+      </ApolloProvider>
+    </AuthProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
