979429b971bae6a1cd0bb8f38de28570520df522
Rob Stevenson <this.rob@protonmail.com>
2025-08-15 13:57:48 +0100

    feat(webapp): implement automatic token refresh link

--- webapp/src/contexts/AuthContext.tsx
+++ webapp/src/contexts/AuthContext.tsx
@@ -1,35 +1,37 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
-import client from '../api/apollo';
 
 interface AuthContextType {
   token: string | null;
+  isRefreshingToken: boolean;
   saveTokens: (newToken: string, newRefreshToken: string) => void;
   logout: () => void;
+  setIsRefreshingToken: (isRefreshing: boolean) => void;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [token, setToken] = useState<string | null>(() => localStorage.getItem('token'));
+  const [isRefreshingToken, setIsRefreshingToken] = useState<boolean>(false);
 
   const saveTokens = (newToken: string, newRefreshToken: string) => {
     localStorage.setItem('token', newToken);
     localStorage.setItem('refreshToken', newRefreshToken);
     setToken(newToken);
-    client.resetStore();
   };
 
   const logout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
     setToken(null);
-    client.resetStore();
   };
 
   const authContextValue = {
     token,
+    isRefreshingToken,
     saveTokens,
     logout,
+    setIsRefreshingToken,
   };
 
   return (
