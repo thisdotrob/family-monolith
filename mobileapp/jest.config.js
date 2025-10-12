167b2ed66bf1bdad7977d9e8e9e0761dd7a570e4
Rob Stevenson <this.rob@protonmail.com>
2025-10-12 20:40:29 +0100

    chore(format): prettier fix mobileapp jest.config.js

--- mobileapp/jest.config.js
+++ mobileapp/jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@babel/runtime)'
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@babel/runtime)',
   ],
   moduleNameMapper: {
     '^@shared/(.*)$': '<rootDir>/../shared/$1',
     '^react$': '<rootDir>/node_modules/react',
-    '^react/jsx-runtime$': '<rootDir>/node_modules/react/jsx-runtime'
+    '^react/jsx-runtime$': '<rootDir>/node_modules/react/jsx-runtime',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleDirectories: ['node_modules', '<rootDir>'],
